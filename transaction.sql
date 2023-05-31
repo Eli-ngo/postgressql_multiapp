db7a0338684a640bf4bc4ea4ad5543f2fa8023d5
Elisabeth Ngo <78852168+Eli-ngo@users.noreply.github.com>
2023-05-31 23:04:27 +0200

    UML diagram

--- transaction.sql
+++ transaction.sql
@@ -1,4 +1,20 @@
+-- Active: 1684762799193@@127.0.0.1@5432@multiapp
 BEGIN;
-SAVEPOINT savepoint,
+
+INSERT INTO forum.forum (title, user_id, description) VALUES
+('Blabla', 2, 'Mouai');
+UPDATE forum.forum SET title = 'hehe' WHERE id = 1;
+
+SELECT * FROM forum.forum;
+
+SAVEPOINT my_savepoint;
+
+INSERT INTO forum.forum (title, user_id, description) VALUES
+('Blabla', 2, 'Mouai');
+
+ROLLBACK TO my_savepoint;
+
+SELECT * FROM forum.forum;
+COMMIT;
 
 END;
